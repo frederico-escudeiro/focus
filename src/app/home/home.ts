8d22951908d5acfd2e0727c67ed888e4b9ee2951
Frederico Escudeiro <frederico.escudeiro@gmail.com>
2025-08-14 18:43:40 +0100

    Adds organised loop for menu items in home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,24 @@
 import { Component } from "@angular/core";
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
+import { MatGridList, MatGridTile } from "@angular/material/grid-list";
 
 @Component({
     selector: 'app-home',
     imports: [
-        
-    ],
+    MatGridList,
+    MatGridTile,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent
+],
     templateUrl: './home.html',
     styleUrl: './home.scss'
 })
 export class Home {
 
+    menuItems = [
+        { title: 'Boards', description: 'Manage your boards and projects', route: '/focus' },
+        { title: 'Profile', description: 'Access and edit your Profile', route: '/settings' }
+    ];
 }
