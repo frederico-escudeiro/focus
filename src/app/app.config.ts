4cb95960b869ff7ba9f5f834cd1f6024a88860d9
Frederico Escudeiro <frederico.escudeiro@gmail.com>
2025-08-19 18:28:26 +0100

    Moves dev firestore settings to environment file.
    - [UNRELATED TO COMMIT] Provided new rules for the database,  to solve the firestore apikey exposure.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,9 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+import { environment } from 'environments/environment';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDA7QptuxwBUiv6FhBSrc8ti4yd_EGAm7U",
-  authDomain: "focus-firebase-project.firebaseapp.com",
-  projectId: "focus-firebase-project",
-  storageBucket: "focus-firebase-project.firebasestorage.app",
-  messagingSenderId: "788770407011",
-  appId: "1:788770407011:web:203815e40a0b50aec7ee56",
-  measurementId: "G-8T72SZ7FDL"
-};
+const firebaseConfig = environment.firebaseConfig;
 
 export const appConfig: ApplicationConfig = {
   providers: [
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDA7QptuxwBUiv6FhBSrc8ti4yd_EGAm7U",
+        authDomain: "focus-firebase-project.firebaseapp.com",
+        projectId: "focus-firebase-project",
+        storageBucket: "focus-firebase-project.firebasestorage.app",
+        messagingSenderId: "788770407011",
+        appId: "1:788770407011:web:203815e40a0b50aec7ee56",
+        measurementId: "G-8T72SZ7FDL"
+    }
+};
